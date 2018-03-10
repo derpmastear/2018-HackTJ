14fe08adb5f8e87166da491f3ff6082d20730cfd
2020vbo <victorbo2020@gmail.com>
2018-03-10 17:59:09 -0500

    sound and detail
    added sound class started stuff on detail

--- DetailEditorMusic.java
+++ DetailEditorMusic.java
@@ -1,7 +1,10 @@
 //Detail editor
 import javax.swing.*;
 public class DetailEditorMusic extends JPanel {
+   private JTextField artist, album;
+   private JLabel folder;
+   private JFileChooser select;
    public DetailEditorMusic() {
-   
+      
    }
 }
