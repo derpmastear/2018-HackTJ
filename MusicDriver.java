4e7b61b0c606618eee5ff000f03101454040e20a
derpmastear <kevinypw@gmail.com>
2018-03-10 15:32:03 -0500

    Changed some shit
    code for main panel and music driver

--- MusicDriver.java
+++ MusicDriver.java
@@ -1,1 +1,17 @@
 //I guess this is a thing. -Kwu
+//imports
+import javax.swing.JFrame;
+
+
+public class MusicDriver
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame("Pirated Music Player");
+      frame.setSize(640, 480);
+      frame.setLocation(100, 50);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new Panel15());
+      frame.setVisible(true);
+   }
+}
--- MusicPanelMain.java
+++ MusicPanelMain.java
@@ -1,1 +1,51 @@
 //Main panel bullshit
+
+/* Code to get all files in a directory and put in array
+
+File folder = new File("your/path");
+File[] listOfFiles = folder.listFiles();
+
+    for (int i = 0; i < listOfFiles.length; i++) {
+      if (listOfFiles[i].isFile()) {
+        System.out.println("File " + listOfFiles[i].getName());
+      } else if (listOfFiles[i].isDirectory()) {
+        System.out.println("Directory " + listOfFiles[i].getName());
+      }
+    }
+  */
+  
+/*********************************************************************
+*Main Panel that contains the other panels.                          *
+*Please note that there are only panels on this main panel.          *
+*********************************************************************/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.CardLayout;
+import java.awt.event.*;
+
+public class MusicPanelMain extends JPanel
+{
+   private DetailEditorMusic details;
+   private SongPlayer songs;
+   private JButton songsTab, detailTab;
+   private JTabbedPane tabs;
+   
+   public MusicPanelMain()
+   {
+      setLayout(new BorderLayout());
+      
+      tabs = new JTabbedPane();
+      
+      details = new DetailEditorMusic();
+      details.setPreferredSize(new Dimension(400, 400));
+      //tabs.add("name goes here", object);
+      
+      songs = new SongPlayer();
+      songs.setPreferredSize(new Dimension(400, 400));
+      //tabs.add("name goes here", object);
+      
+      add(tabs, BorderLayout.CENTER);
+   }
+}
+
