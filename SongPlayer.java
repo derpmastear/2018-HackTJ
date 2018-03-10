df4af5bf2616709ac8d7e8abefb8a8e30eb34eb1
derpmastear <kevinypw@gmail.com>
2018-03-10 16:18:45 -0500

    added song code

--- SongPlayer.java
+++ SongPlayer.java
@@ -1,7 +1,24 @@
 //Song platter
 import javax.swing.*;
-public class SongPlayer extends JPanel {
-   public SongPlayer() {
+public class SongPlayer extends JPanel 
+{
+   private String currentSong;
+   private File[] listOfFiles;
+   public SongPlayer() 
+   {
+      
+   }
+   public static getSongs()
+   {
+      File folder = new File("your/path");
+      File[] listOfFiles = folder.listFiles();
    
+      for (int i = 0; i < listOfFiles.length; i++) {
+         if (listOfFiles[i].isFile()) {
+            System.out.println("File " + listOfFiles[i].getName());
+         } else if (listOfFiles[i].isDirectory()) {
+            System.out.println("Directory " + listOfFiles[i].getName());
+         }
+      }
    }
 }
