36d31c82336da890ab0b3c10926a716d8b081701
2020vbo <victorbo2020@gmail.com>
2018-03-10 21:28:39 -0500

    formatted editor a little

--- DetailEditorMusic.java
+++ DetailEditorMusic.java
@@ -8,9 +8,15 @@ public class DetailEditorMusic extends JPanel {
    private JLabel folder;
    private JFileChooser select;
    public DetailEditorMusic() {
+      setLayout(new BorderLayout());
+      JPanel northPanel = new JPanel();
+      add(northPanel, BorderLayout.NORTH);
+      folder.setText("Folder directory");
+      northPanel.add(folder);
+      northPanel.setLayout(new GridLayout(1, 2));
       find = new JButton("Find Folder Directory");
       find.addActionListener(new FileListener());
-      add(find);
+      northPanel.add(find);
       select = new JFileChooser("C:");
    }
    private class FileListener implements ActionListener {
