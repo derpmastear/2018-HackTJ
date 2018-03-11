e739ecd4dc8936f2067f9a1879a19806afd689ac
2020vbo <victorbo2020@gmail.com>
2018-03-10 23:46:19 -0500

    compiles and shit

--- DetailEditorMusic.java
+++ DetailEditorMusic.java
@@ -9,14 +9,19 @@ public class DetailEditorMusic extends JPanel {
    private JFileChooser select;
    public DetailEditorMusic() {
       setLayout(new BorderLayout());
+      
       JPanel northPanel = new JPanel();
-      add(northPanel, BorderLayout.NORTH);
-      folder.setText("Folder directory");
-      northPanel.add(folder);
       northPanel.setLayout(new GridLayout(1, 2));
+      add(northPanel, BorderLayout.NORTH);
+      
+      folder = new JLabel("Folder directory:");
+      northPanel.add(folder);
+      
       find = new JButton("Find Folder Directory");
       find.addActionListener(new FileListener());
+      find.setHorizontalAlignment(JLabel.CENTER);
       northPanel.add(find);
+      
       select = new JFileChooser("C:");
    }
    private class FileListener implements ActionListener {
