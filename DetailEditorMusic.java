caef83f8a5d2f79d3e3a9f733ea2f29fb8e543c4
2020vbo <victorbo2020@gmail.com>
2018-03-10 20:13:35 -0500

    FileChooser works
    title says it all

--- DetailEditorMusic.java
+++ DetailEditorMusic.java
@@ -1,10 +1,21 @@
 //Detail editor
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 public class DetailEditorMusic extends JPanel {
    private JTextField artist, album;
+   private JButton find;
    private JLabel folder;
    private JFileChooser select;
    public DetailEditorMusic() {
-      
+      find = new JButton("Find Folder Directory");
+      find.addActionListener(new FileListener());
+      add(find);
+      select = new JFileChooser("C:");
+   }
+   private class FileListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         select.showOpenDialog(find);
+      }
    }
 }
